0d00bfefda58961f93706a8d88c1880c5e549429
Hermanda Mak <hermandamak@gmail.com>
2022-07-28 14:37:33 -0700

    Update minor changes and styling

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -7,18 +7,23 @@ const UserLogin = (props) => {
   return (
     <div className='vh-100 d-flex align-items-center'>
       <div className='col-lg-7 col-10 mx-auto'>
-        <Form className=''>
+        <h2 className='mb-5'>Log In</h2>
+        <Form className='mb-5'>
           <Form.Group className='mb-3' controlId='email'>
             <Form.Label>Email address</Form.Label>
             <Form.Control type='email' name='email' />
           </Form.Group>
-          <Form.Group className='mb-3' controlId='password'>
+          <Form.Group className='mb-5' controlId='password'>
             <Form.Label>Password</Form.Label>
-            <Form.Control type='password' name='name' />
+            <Form.Control type='password' name='password' />
           </Form.Group>
-          <Button variant="outline-dark" type="submit">Submit</Button>
+          <div className='d-grid d-md-block'>
+            <Button variant="outline-dark" type="submit" className='col-md-5'>Log In</Button>
+          </div>
         </Form>
-        <div className='align-self-end'>No Account? <Nav.Link as={Link} to={"/register"} className={"d-inline text-primary"}>Create Now</Nav.Link></div>
+        <div className='d-flex justify-content-end'>
+          <div>No Account? <Nav.Link as={Link} to={"/register"} className={"d-inline text-primary fw-bold"}>Create Now</Nav.Link></div>
+        </div>
       </div>
     </div>
   )
