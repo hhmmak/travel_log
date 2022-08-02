b3efe31f1ce31463490a9779b6bab6b8b2bcc83e
Hermanda Mak <hermandamak@gmail.com>
2022-08-01 23:53:16 -0700

    add POST request route

--- server/flask_app/controllers/posts.py
+++ server/flask_app/controllers/posts.py
@@ -1,6 +1,8 @@
-from flask import jsonify
+from flask import jsonify, request, json
+from flask_bcrypt import Bcrypt
 from flask_app import app
 from flask_app.models import post
+from datetime import datetime
 
 #.. GET routes
 @app.route('/api/posts/<int:id>')
@@ -12,6 +14,25 @@ def post_detail(id):
     return jsonify(post_info)
 
 #.. POST routes
+@app.route('/api/posts', methods=['POST'])
+def user_create():
+    dataJSON = request.get_json()
+    print("=============================================   dataJSON: ", dataJSON)
+    duration = datetime.strptime(dataJSON['dateTo'], '%Y-%m-%d')-datetime.strptime(dataJSON['dateFrom'], '%Y-%m-%d')
+    data = {
+        "title": dataJSON['title'],
+        "content": dataJSON['content'],
+        "itinerary": dataJSON['itinerary'],
+        "destination": dataJSON['destination'],
+        "country": dataJSON['country'],
+        "date_from": dataJSON['dateFrom'],
+        "date_to": dataJSON['dateTo'],
+        "duration": duration.days + 1,
+        "user_id": dataJSON['user_id']
+    }
+    post.Post.add_post(data)
+    print("---------data: ", data, "----------------")
+    return jsonify({"message": "post created"})
 
 #.. PUT routes
 
