22bf9f4fdb1790f0129b6829c921d57cdc88cc89
Hermanda Mak <hermandamak@gmail.com>
2022-08-01 23:52:59 -0700

    change get_post and add_post methods

--- server/flask_app/models/post.py
+++ server/flask_app/models/post.py
@@ -2,7 +2,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 
 class Post:
 
-    db_name = 'travel_log_schema'
+    db_name = 'travel_schema'
 
     def __init__(self, data):
         self.id = data['id']
@@ -31,15 +31,33 @@ class Post:
     
     @classmethod
     def get_post(cls, data):
-        query = "SELECT * FROM users LEFT JOIN posts ON users.id = posts.user_id LEFT JOIN locations ON posts.location_id = locations.id LEFT JOIN countries ON locations.country_id = countries.id WHERE posts.id = %(id)s;"
+        # with location/country table
+        # query = "SELECT * FROM users LEFT JOIN posts ON users.id = posts.user_id LEFT JOIN locations ON posts.location_id = locations.id LEFT JOIN countries ON locations.country_id = countries.id WHERE posts.id = %(id)s;"
+        # without location/country table
+        query = "SELECT * FROM users LEFT JOIN posts ON users.id = posts.user_id WHERE posts.id = %(id)s;"
         results = connectToMySQL(cls.db_name).query_db(query, data)
-        return cls(results[0])
+        print("=======results:", results)
+        post = {
+            "user_id": results[0]['user_id'],
+            "username": results[0]['username'],
+            "id": results[0]['posts.id'],
+            "title": results[0]['title'],
+            "content": results[0]['content'],
+            "itinerary": results[0]['itinerary'],
+            "location": results[0]['location'],
+            "duration": results[0]['duration'],
+            "country": results[0]['country'],
+            "date_from": results[0]['date_from'],
+            "date_to": results[0]['date_to'],
+            "posts.created_at": results[0]['posts.created_at']
+        }
+        return post
 
 
     #.. add methods
     @classmethod
     def add_post (cls,data):
-        query = "INSERT INTO posts (title, content, itinerary, location_id, user_id, date_from, date_to, duration) VALUES (%(title)s, %(content)s, %(itinerary)s, %(location_id)s, %(user_id)s, %(date_from)s, %(date_to)s, %(duration)s);"
+        query = "INSERT INTO posts (title, content, itinerary, destination, country, user_id, date_from, date_to, duration) VALUES (%(title)s, %(content)s, %(itinerary)s, %(destination)s, %(country)s, %(user_id)s, %(date_from)s, %(date_to)s, %(duration)s);"
         return connectToMySQL(cls.db_name).query_db(query, data)
 
     #.. update methods
