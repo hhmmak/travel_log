f46fbc518d736e44822d35503981aa212b95258c
Hermanda Mak <hermandamak@gmail.com>
2022-08-01 23:08:15 -0700

    add form for create new post

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,39 +1,69 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 
 const PostForm = (props) => {
 
+  const navigate = useNavigate();
+  const {userId} = props;
+
+  const [post, setPost] = useState({
+    title: "",
+    destination: "",
+    country: "",
+    dateFrom: "",
+    dateTo: "",
+    itinerary: "",
+    content: "",
+    userId: userId
+  })
+
+  const onChangeHandler = (e) => {
+    setPost({...post, [e.target.name]: e.target.value });
+  }
+  
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(post)
+    axios.post("http://localhost:5000/api/posts", post)
+      .then(res => navigate('/'))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div>
-      <Form className='mb-5'>
+      <Form className='mb-5' onSubmit={onSubmitHandler}>
         <Form.Group className='mb-3' controlId='title'>
           <Form.Label>Title</Form.Label>
-          <Form.Control type='text' name='firstName' />
+          <Form.Control type='text' name='title' onChange={onChangeHandler}/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='destination'>
           <Form.Label>Destination</Form.Label>
-          <Form.Control type='text' name='destination' />
+          <Form.Control type='text' name='destination' onChange={onChangeHandler}/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='country'>
           <Form.Label>Country</Form.Label>
-          <Form.Control type='text' name='country' />
+          <Form.Control type='text' name='country' onChange={onChangeHandler}/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='dateFrom'>
           <Form.Label>From</Form.Label>
-          <Form.Control type='date' name='dateFrom' />
+          <Form.Control type='date' name='dateFrom' onChange={onChangeHandler}/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='dateTo'>
           <Form.Label>To</Form.Label>
-          <Form.Control type='date' name='dateTo' />
+          <Form.Control type='date' name='dateTo' onChange={onChangeHandler}/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='itinerary'>
           <Form.Label>Itinerary</Form.Label>
-          <Form.Control as="textarea" rows={5} name='itinerary' />
+          <Form.Control as="textarea" rows={5} name='itinerary' onChange={onChangeHandler}/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='content'>
           <Form.Label>Log</Form.Label>
-          <Form.Control as="textarea" rows={5} name='content' />
+          <Form.Control as="textarea" rows={5} name='content' onChange={onChangeHandler}/>
         </Form.Group>
         <div className='d-grid d-md-block'>
           <Button variant="outline-dark" type="submit" className='col-md-5'>Create</Button>
@@ -42,4 +72,4 @@ const PostForm = (props) => {
     </div>
   )
 }
-export default PostForm
+export default PostForm;
