76dfe22150f738c5e3686f356199e6b69c886abe
Hermanda Mak <hermandamak@gmail.com>
2022-08-04 11:47:18 -0700

    connect with backend

--- client/src/components/UserRegister.js
+++ client/src/components/UserRegister.js
@@ -1,35 +1,59 @@
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 const UserRegister = (props) => {
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    axios.post(`http://localhost:5000/api/users`, user)
+      .then(res => navigate('/'))
+      .catch(err => console.log(err))
+  }
+
+  const onChangeHandler = (e) => {
+    setUser({...user, [e.target.name]: e.target.value})
+  }
+
+
+
   return (
     <div className='vh-75 d-flex align-items-center'>
       <div className='col-lg-7 col-10 mx-auto'>
         <h2 className='mb-3'>Create Account</h2>
           <p className='d-flex justify-content-end text-danger fs-6 fw-light'>*All fields required</p>
-        <Form className='mb-5'>
+        <Form className='mb-5' onSubmit={ onSubmitHandler }>
           <Form.Group className='mb-3' controlId='username'>
             <Form.Label>Username</Form.Label>
-            <Form.Control type='text' name='username' />
+            <Form.Control type='text' name='username' onChange={ onChangeHandler }/>
           </Form.Group>
           <Form.Group className='mb-3' controlId='firstName'>
             <Form.Label>First name</Form.Label>
-            <Form.Control type='text' name='firstName' />
+            <Form.Control type='text' name='firstName' onChange={ onChangeHandler }/>
           </Form.Group>
           <Form.Group className='mb-3' controlId='lastName'>
             <Form.Label>Last name</Form.Label>
-            <Form.Control type='text' name='lastName' />
+            <Form.Control type='text' name='lastName' onChange={ onChangeHandler }/>
           </Form.Group>
           <Form.Group className='mb-3' controlId='email'>
             <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' name='email' placeholder='example@travel-log.com' />
+            <Form.Control type='email' name='email' placeholder='example@travel-log.com' onChange={ onChangeHandler }/>
           </Form.Group>
-          <Form.Group className='mb-5' controlId='password'>
+          <Form.Group className='mb-3' controlId='password'>
             <Form.Label>Password</Form.Label>
-            <Form.Control type='password' name='password' />
+            <Form.Control type='password' name='password' onChange={ onChangeHandler }/>
             <Form.Text className='text-muted'>Minimum 6 characters</Form.Text>
+          </Form.Group>
+          <Form.Group className='mb-5' controlId='confirmPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type='password' name='confirmPassword' onChange={ onChangeHandler }/>
           </Form.Group>
           <div className='d-grid d-md-block'>
             <Button variant="outline-dark" type="submit" className='col-md-5'>Create Account</Button>
