76a4afa85deeec92ef6121bdc3dae89921bb847d
Hermanda Mak <hermandamak@gmail.com>
2022-08-05 19:56:36 -0700

    add jwt for login validation

--- server/flask_app/controllers/users.py
+++ server/flask_app/controllers/users.py
@@ -1,7 +1,14 @@
-from flask import jsonify, request, make_response, abort
-from flask_bcrypt import Bcrypt
+from flask import jsonify, request, make_response
+
 from flask_app import app
 from flask_app.models import user
+
+#authentication related
+from flask_bcrypt import Bcrypt
+import jwt
+import os
+import datetime
+
 bcrypt = Bcrypt(app)
 
 
@@ -25,7 +32,7 @@ def user_account_by_id(id_num):
     return jsonify(user_info)
 
 #.. POST routes
-@app.route('/api/users/login', methods=['GET'])
+@app.route('/login', methods=['POST'])
 def user_login():
     dataJSON = request.get_json()
     # print("=============================================   dataJSON: ", dataJSON)
@@ -33,11 +40,21 @@ def user_login():
         "email": dataJSON['email'],
     }
     user_info = user.User.get_user_by_email(data)
-    if not user_info or not bcrypt.check_password_hash(user_info.password, dataJSON['password']):
-        return jsonify({"user": "invalid", "status": "invalid login"})
+    # print("=============================================   user_info['password']: ", user_info['password'])
 
+    # login not success
+    if not user_info or not bcrypt.check_password_hash(user_info['password'], dataJSON['password']):
+        return make_response('could not verify', 401, {'Authentication': 'Login required'})
 
-    return jsonify({"user": user_info, "status": "login success"})
+    # login success, return 
+    payload = {
+        "email": user_info['email'],
+        "username": ['username'],
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    }
+    jwt_key = os.environ.get("TOKEN_KEY")
+    token = jwt.encode(payload, key=jwt_key, algorithm="HS256")
+    return jsonify({"token": token, "userId": user_info['id']})
 
 @app.route('/api/users', methods=['POST'])
 def user_create():
