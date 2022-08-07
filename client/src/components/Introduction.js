48a3a9e63ec6da9e72d03cf9d0bb4de20a449205
Hermanda Mak <hermandamak@gmail.com>
2022-08-07 04:15:50 -0700

    initial commit

---
+++ client/src/components/Introduction.js
@@ -0,0 +1,8 @@
+const Introduction = (props) => {
+  return (
+    <div className="d-flex justify-content-center my-3 p-5 bg-white rounded-4">
+      <p className="fs-2 text-center">Remember your vacation<br />Share your plan<br />Find your next journey</p>
+    </div>
+  )
+}
+export default Introduction;
