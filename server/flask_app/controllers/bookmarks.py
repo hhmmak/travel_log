544ff41e22cac5de7bb2004bb764039e229df6e6
Hermanda Mak <hermandamak@gmail.com>
2022-08-06 23:03:49 -0700

    add jwt authorization and get bookmark method

--- server/flask_app/controllers/bookmarks.py
+++ server/flask_app/controllers/bookmarks.py
@@ -1,9 +1,22 @@
 from flask import jsonify, request
 from flask_app import app
 from flask_app.models import bookmark
+from flask_app.decorators import jwt_required
+
+#.. POST route - get bookmark list
+@app.route('/api/bookmarks/list', methods=['POST'])
+@jwt_required
+def bookmark_list_by_user():
+    dataJSON = request.get_json()
+    data ={
+        "user_id": dataJSON['userId']
+    }
+    bookmark_list = bookmark.Bookmark.get_bookmarks_by_user(data)
+    return jsonify(bookmark_list)
 
 #.. POST route - add bookmark
 @app.route('/api/bookmarks', methods=['POST'])
+@jwt_required
 def bookmark_create():
     dataJSON = request.get_json()
     data = {
@@ -15,6 +28,7 @@ def bookmark_create():
 
 #.. DELETE route - remove bookmark
 @app.route('/api/bookmarks', methods=['DELETE'])
+@jwt_required
 def bookmark_delete():
     dataJSON = request.get_json()
     data = {
