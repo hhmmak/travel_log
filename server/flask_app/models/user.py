831dba6f3bc4172a10ad37e9d2873f6d91cfe9bd
Hermanda Mak <hermandamak@gmail.com>
2022-10-05 21:55:23 -0700

    comment out class instance variable

--- server/flask_app/models/user.py
+++ server/flask_app/models/user.py
@@ -8,17 +8,17 @@ class User:
 
     db_name = 'travel_log_schema'
 
-    def __init__(self, data):
-        self.id = data['id']
-        self.username = data['username']
-        self.first_name = data ['first_name']
-        self.last_name = data ['last_name']
-        self.email = data ['email']
-        self.password = data ['password']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
-        self.posts = []
-        self.bookmarks = []
+    # def __init__(self, data):
+    #     self.id = data['id']
+    #     self.username = data['username']
+    #     self.first_name = data ['first_name']
+    #     self.last_name = data ['last_name']
+    #     self.email = data ['email']
+    #     self.password = data ['password']
+    #     self.created_at = data['created_at']
+    #     self.updated_at = data['updated_at']
+    #     self.posts = []
+    #     self.bookmarks = []
 
     #.. get methods
     @classmethod
