b9228b6bc6f0ba099a23f46293da1e3409689f6a
Hermanda Mak <hermandamak@gmail.com>
2022-10-06 02:09:52 -0700

    add future post_create pseudocode

--- server/flask_app/controllers/posts.py
+++ server/flask_app/controllers/posts.py
@@ -20,7 +20,6 @@ def post_detail(id):
     return jsonify(post_info)
 
 #.. POST routes
-
 @app.route('/api/posts', methods=['POST'])
 @jwt_required
 def post_create():
@@ -35,6 +34,7 @@ def post_create():
     # continue if validation passed
     data = {
         "title": validation['title'] if 'title' in validation else dataJSON['title'],
+        # "title": dataJSON['title'],   # api testing purpose
         "content": dataJSON['content'],
         "itinerary": dataJSON['itinerary'],
         "destination": dataJSON['destination'],
@@ -42,11 +42,28 @@ def post_create():
         "date_from": dataJSON['dateFrom'],
         "date_to": dataJSON['dateTo'],
         "duration": validation['duration'].days,
+        # "duration":dataJSON['duration'],    # api testing purpose
         "user_id": dataJSON['user_id']
     }
-    post.Post.add_post(data)
+    result = post.Post.add_post(data)
     # print("--------- data: ", data, "----------------")
-    return jsonify({"message": "post created"})
+    return jsonify({"message": "post created", "id": result})
+
+# TODO post_create() =>
+    # obtain raw data including country, city, location data from form
+    # check if city/location exists from dataJSON = only country
+        # if exists, check if city/location exists from table
+            # if exists, check if destination exists
+                # if exists, obtain destination_id
+                # if not, add into destination table; NULL for empty city/location; obtain destination_id
+            # if not, add into table
+                # add into destination table; NULL for empty city/location; obtain destination_id
+        # if not, check if destination = country exists
+            # if exists, obtain destination_id
+            # if not, add into destination table w/ destination = country; obtain destination_id
+    # create data from dataJSON, validaiton and destination_id
+# TODO complete
+    
 
 #.. PUT routes
 @app.route('/api/posts/<int:id>', methods=['PUT'])
@@ -69,7 +86,9 @@ def post_edit(id):
         "country": dataJSON['country'],
         "date_from": dataJSON['dateFrom'],
         "date_to": dataJSON['dateTo'],
-        "duration": validation['duration'].days,
+        "duration": validation['duration'].days
+        # "duration":dataJSON['duration'],    # api testing purpose
+        # "user_id": dataJSON['userId']       # api testing purpose
     }
     # print("---------data: ", data, "----------------")
     post.Post.update_post(data)
