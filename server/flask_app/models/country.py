531ad489ba25db72fd8575d4ebea4eb75687609a
Hermanda Mak <hermandamak@gmail.com>
2022-10-06 02:10:17 -0700

    remove class instance variable

--- server/flask_app/models/country.py
+++ server/flask_app/models/country.py
@@ -4,12 +4,6 @@ class Country:
 
     db_name = 'travel_log_schema'
 
-    def __init__(self, data):
-        self.id = data['id']
-        self.country = data['country']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
-
     #.. get methods
     @classmethod
     def get_all_countries(cls):
