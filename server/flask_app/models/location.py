64b1e328d523c43b1ae743712a17a820fe6b59a0
Hermanda Mak <hermandamak@gmail.com>
2022-10-06 01:00:21 -0700

    initial commit

---
+++ server/flask_app/models/location.py
@@ -0,0 +1,23 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Location:
+
+    db_name = 'travel_log_schema'
+
+    #.. get methods
+    @classmethod
+    def get_location_by_name(cls,data):
+        query = "SELECT * FROM locations WHERE name = %(name)s;"
+        results = connectToMySQL(cls.db_name).query_db(query,data)
+        location = {
+            "location": results[0]['name']
+        }
+        return location
+
+
+    #.. add methods
+    @classmethod
+    def add_location(cls,data):
+        query = "INSERT INTO locations (name) VALUE (%(location)s);"
+        result = connectToMySQL(cls.db_name).query_db(query,data)
+        return result
