024a960e96b510e1691564f6f93b2d1387ef4bde
Hermanda Mak <hermandamak@gmail.com>
2022-10-06 01:08:45 -0700

    change get method return data

--- server/flask_app/models/location.py
+++ server/flask_app/models/location.py
@@ -10,7 +10,7 @@ class Location:
         query = "SELECT * FROM locations WHERE name = %(name)s;"
         results = connectToMySQL(cls.db_name).query_db(query,data)
         location = {
-            "location": results[0]['name']
+            "id": results[0]['id']
         }
         return location
 
