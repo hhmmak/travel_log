a499cbf6cc5d0f906e50f8e4adf6dcc07eac7921
Hermanda Mak <hermandamak@gmail.com>
2022-10-05 22:51:49 -0700

    change y-axis margin for p-tag

--- client/src/components/Introduction.js
+++ client/src/components/Introduction.js
@@ -3,9 +3,9 @@ import './css/Introduction.css'
 const Introduction = (props) => {
   return (
     <div className="introduction-container d-flex flex-column my-3 p-5 rounded-4">
-      <p className="fs-2 text-center">Remember your vacation</p>
-      <p className="fs-2 text-center">Share your plan</p>
-      <p className="fs-2 text-center">Find your next journey</p>
+      <p className="fs-1 text-center my-2">Remember your vacation</p>
+      <p className="fs-1 text-center my-1">Share your plan</p>
+      <p className="fs-1 text-center my-2">Find your next journey</p>
     </div>
   )
 }
