84f37ef585f7e0eea4ce31dcde159a6f173d0a53
Hermanda Mak <hermandamak@gmail.com>
2022-10-06 01:12:50 -0700

    initial commit

---
+++ server/flask_app/models/city.py
@@ -0,0 +1,23 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class City:
+
+    db_name = 'travel_log_schema'
+
+    #.. get methods
+    @classmethod
+    def get_city_by_name(cls,data):
+        query = "SELECT * FROM locations WHERE name = %(name)s;"
+        results = connectToMySQL(cls.db_name).query_db(query,data)
+        location = {
+            "cityId": results[0]['id']
+        }
+        return location
+
+
+    #.. add methods
+    @classmethod
+    def add_city(cls,data):
+        query = "INSERT INTO locations (name) VALUE (%(city)s);"
+        result = connectToMySQL(cls.db_name).query_db(query,data)
+        return result
