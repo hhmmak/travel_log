5b94fa0040cc78022dc75c57fb88fd7b9e38c493
Hermanda Mak <hermandamak@gmail.com>
2023-01-06 15:59:47 -0800

    update import file paths

--- client/src/features/post-form/PostAdd.js
+++ client/src/features/post-form/PostAdd.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-import FormDefault from '.form-default/FormDefault';
+import FormDefault from './form-default/FormDefault';
 
 
 const PostAdd = () => {
