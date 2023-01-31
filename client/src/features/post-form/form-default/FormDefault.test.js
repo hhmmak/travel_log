37bfc965b0d0c82fd32a01536a7f1d6026061ae2
Hermanda Mak <hermandamak@gmail.com>
2023-01-30 23:29:24 -0800

    add test for form input fields

--- client/src/features/post-form/form-default/FormDefault.test.js
+++ client/src/features/post-form/form-default/FormDefault.test.js
@@ -1,5 +1,4 @@
 import FormDefault from './FormDefault';
-
 import {render, screen, cleanup} from '@testing-library/react';
 
 afterEach(() => {
@@ -20,3 +19,22 @@ test('should have submit button with "Post" as text', () => {
   expect(submitButtonElement).toHaveTextContent('Post');
 
 });
+
+test('should display all form input field', () => {
+  const error = {};
+  const post = {};
+  const setPost = jest.fn();
+  const submitAction = jest.fn();
+  const submitText = "Post";
+
+  const textboxNameList = ["title", "location", "city", "itinerary", "content"]
+
+  render(<FormDefault error={error} post={post} setPost={setPost} submitAction={submitAction} submitText={submitText}/>)
+  const inputTextElements = screen.getAllByRole('textbox');
+  expect(inputTextElements.map((ele => ele.name))).toEqual(textboxNameList);
+  const countryElement = screen.getByRole('combobox');
+  const countryListElement = screen.getAllByRole('option');
+  expect(countryElement).toBeInTheDocument();
+  expect(countryListElement).toHaveLength(1);
+  expect(countryListElement[0]).toHaveTextContent("Select one country");
+})
