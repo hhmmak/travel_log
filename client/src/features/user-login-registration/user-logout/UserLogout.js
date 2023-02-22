5b555f6b3fc130faad2bf591708f9fa17ad673a0
Hermanda Mak <hermandamak@gmail.com>
2023-02-21 16:17:07 -0800

    update useEffect

--- client/src/features/user-login-registration/user-logout/UserLogout.js
+++ client/src/features/user-login-registration/user-logout/UserLogout.js
@@ -1,5 +1,21 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const UserLogout = () => {
+
+  const nav = useNavigate();
+
+  useEffect(() => {
+    const returnToHome = setTimeout(() => {
+      nav('/');
+    }, 10000)
   
+    return () => {
+      clearTimeout(returnToHome);
+    }
+  }, [])
+  
+
   return (
     <div>
       <p>Successful Logout!</p>
